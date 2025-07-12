9263df2920de7a02ff5a2f6a8c219319888c1e6f
shem958 <shemgikunda37@gmail.com>
2025-07-12 23:53:19 +0300

    refactor: simplify App component structure and remove unused code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import JobList from "./JobList";
+import JobForm from "./JobForm";
+import JobDetail from "./JobDetail";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<JobList />} />
+        <Route path="/add" element={<JobForm />} />
+        <Route path="/edit/:id" element={<JobForm />} />
+        <Route path="/jobs/:id" element={<JobDetail />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
